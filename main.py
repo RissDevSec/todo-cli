b5580bae746d073ed9752457ff3e6a9df03634bf
FarisArhana <ahmadfarisarhana@gmail.com>
2026-08-26 08:41:48 +0700

    feat: add mark-as-done command and migrate to JSON storage

--- main.py
+++ main.py
@@ -1,45 +1,73 @@
 import os
 import sys
+import json
 
-TASKS_FILE = "tasks.txt"
+TASKS_FILE = "tasks.json"
 
 def load_tasks():
-    tasks = []
-    if os.path.exists(TASKS_FILE): 
+    try:
         with open(TASKS_FILE, "r") as file:
-            for task in file:
-                tasks.append(task.strip())
-    return tasks
+            return json.load(file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return []
+
+def save_tasks(tasks):
+    with open(TASKS_FILE, "w") as file:
+        json.dump(tasks, file, indent="\t")
 
 def add_task(task):
-    with open(TASKS_FILE, "a") as file:
-        file.write(f"{task}\n")
-    print(f"\'{task}\' has been added.")
+    tasks = load_tasks()
+    tasks.append({
+        "task": task,
+        "done": False
+    })
+    save_tasks(tasks)
+    print(f"'{task}' has been added.")
+
+def mark_task_done(index):
+    tasks = load_tasks()
+    if not tasks: return print("There are no tasks yet.")
+    try:
+        if index <= 0: raise IndexError
+        task = tasks[index - 1]
+        if task['done']:
+            task['done'] = False
+            print(f"'{task['task']}' has been marked as not done.")
+        else:
+            task['done'] = True
+            print(f"'{task['task']}' has been marked as done.")
+    except IndexError:
+        return print("Invalid task number.")
+    else:
+        save_tasks(tasks)
+
 
 def list_tasks():
     tasks = load_tasks()
     if not tasks: return print("There are no tasks yet.")
     print("\nTasks:")
     for index, task in enumerate(tasks, start=1): 
-        print(f"{index}. {task}")
+        done = "[x]" if task['done'] else "[ ]"
+        print(f"{index}. {done} {task['task']}")
 
 def delete_task(index):
     tasks = load_tasks()
     if not tasks: return print("There are no tasks yet.")
     try:
-        print(f"\'{tasks.pop(int(index) - 1)}\' has been deleted.")
+        if index <= 0: raise IndexError
+        task = tasks.pop(index - 1)['task']
+        print(f"'{task}' has been deleted.")
     except IndexError:
         print("Invalid task number.")
     else:
-        with open(TASKS_FILE, "w",) as file:
-            for task in tasks:
-                file.write(f"{task}\n")
+        save_tasks(tasks)
 
 def main():
     while True:
         print("\ntodo-cli — a simple task manager\n")
         print("Commands:")
         print(" add <task>")
+        print(" done <index>")
         print(" list")
         print(" delete <index>")
         print(" exit")
@@ -55,6 +83,14 @@ def main():
                         add_task(" ".join(command[1:]))
                     else:
                         print("Please specify a task. Usage: add <task>")
+                case "done":
+                    if len(command) > 1:
+                        try:
+                            mark_task_done(int(command[1]))
+                        except ValueError:
+                            print("Please enter a valid number.")
+                    else:
+                        print("Please specify a task number. Usage: done <index>")
                 case "list":
                     list_tasks()
                 case "delete":
